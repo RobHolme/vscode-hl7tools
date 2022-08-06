999c4c6b186a82eb9cc88243d3ac4bb469ac1b07
Rob Holme <rob@holme.com.au>
2022-08-06 15:35:47 +0800

    converted 'extractFieldResult' to typescript

--- src/ExtractFieldResult.ts
+++ src/ExtractFieldResult.ts
@@ -8,9 +8,12 @@
 //----------------------------------------------------
 // class defining a single field value and the filename it was found in
 class result {
+	private _filename: string;
+	private _value: string;
+
 	// @param {string} Filename - the name of the file that the value was found in
 	// @param {string} Value - a field value 
-	constructor(Filename, Value) {
+	constructor(Filename: string, Value: string) {
 		this._filename = Filename;
 		this._value = Value;
 	}
@@ -27,13 +30,17 @@ class result {
 //----------------------------------------------------
 // a class containing a collection of results.
 class resultCollection {
+	private _resultItems: result[];
+	private _maxLength: number;
+
 	constructor() {
 		this._resultItems = [];
 		this._maxLength = 5;  // the title of the value field is 5 characters, so the length should be at least 5 if all values are smaller
 	}
+
 	// add a new result object to this collection.
 	// @param {object} resultItem - the result object to add to the result collection
-	AddResult(resultItem) {
+	public AddResult(resultItem: result) {
 		this._resultItems.push(resultItem);
 		if (resultItem.Value.length > this._maxLength) {
 			this._maxLength = resultItem.Value.length;
@@ -49,6 +56,3 @@ class resultCollection {
 		return this._maxLength;
 	}
 }
-
-exports.result = result;
-exports.resultCollection = resultCollection;
