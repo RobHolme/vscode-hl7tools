e32fcde9449f9a1e18a240a1903605a51c4c86a9
Rob Holme <rob@holme.com.au>
2022-08-07 16:14:08 +0800

    converted to typescript

--- src/CursorManager.ts
+++ src/CursorManager.ts
@@ -27,7 +27,7 @@ export abstract class CursorManager {
 	//----------------------------------------------------
 	// set the position of the cursor in the document, selecting a range of text. Reveal the cursor position (if not in the viewport).
 	// @param {FindFieldResult} FieldLocation - the line number of the field, and the start and end character positions of the field
-	public static SetCursorPosition(FieldLocation: FindFieldResult) {
+	public static SetCursorPosition(FieldLocation: FindFieldResult) : void {
 		const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
 		var startPosition: vscode.Position = new vscode.Position(FieldLocation.Line, FieldLocation.StartCharacter);
 		var endPosition: vscode.Position = new vscode.Position(FieldLocation.Line, FieldLocation.EndCharacter);
