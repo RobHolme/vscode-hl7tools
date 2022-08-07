ce121b9f2620886436b682e41dcd11faf3ac5988
Rob Holme <rob@holme.com.au>
2022-08-07 16:45:47 +0800

    converted to typescript

--- src/ExtractFields.ts
+++ src/ExtractFields.ts
@@ -5,24 +5,30 @@
 */
 
 // load modules
-const vscode = require('vscode');
-const common = require('./common.js');
-const resultClass = require('./ExtractFieldResult.js');
+import * as vscode from 'vscode';
+import { Util } from './Util'; 
+import { Result, ResultCollection } from './ExtractFieldResult';
+
 
 // return codes for ExtractAllFields()
-const ExtractReturnCode = {
-	ERROR_NO_LOCATION_PROVIDED: 0,
-	ERROR_LOCATION_NOT_VALID: 1,
-	SUCCESS: 2
+export enum ExtractReturnCode {
+	ERROR_NO_LOCATION_PROVIDED = 0,
+	ERROR_LOCATION_NOT_VALID = 1,
+	SUCCESS = 2
 }; 
+
 
 //----------------------------------------------------
 // extract the index of a field location from the hl7 item location string
 // @param {string} itemLocation - A string value identifying the field location to extract (e.g. PID-3)
-function ExtractAllFields(itemLocation) {
-	var results = new resultClass.resultCollection();
-	var fieldIndex;
-	var segmentName = "";
+export function ExtractAllFields(itemLocation: string | undefined) :ExtractReturnCode {
+	if (itemLocation === undefined) {
+		return ExtractReturnCode.ERROR_LOCATION_NOT_VALID
+	}
+	
+	var results = new ResultCollection();
+	var fieldIndex: number | null;
+	var segmentName: string = "";
 
 	// return if no location provided by the user;
 	if (!itemLocation) {
@@ -37,30 +43,30 @@ function ExtractAllFields(itemLocation) {
 	}
 
 	// identify the segment name and field index from the location string
-	segmentName = common.GetSegmentNameFromLocationString(itemLocation).toUpperCase();
-	fieldIndex = parseInt(common.GetFieldIndex(itemLocation), 10);
+	segmentName = Util.GetSegmentNameFromLocationString(itemLocation).toUpperCase();
+	fieldIndex = Util.GetFieldIndex(itemLocation);
+	if (fieldIndex == null) {
+		return ExtractReturnCode.ERROR_LOCATION_NOT_VALID;
+	}
 	// the first field of MSH, FHS, and BHS segments is the field delimiter, adjust index accordingly for these fields
 	if (segmentName == 'MSH' || segmentName == 'FHS' || segmentName == 'BHS') {
 		fieldIndex--;
 	}
 	
 	// extract the field(s) from the current document
-	results = common.GetFields(segmentName, fieldIndex);
+	results = Util.GetFields(segmentName, fieldIndex);
 	
 	// display the results to on output window
-	var allResults = results.Results;
+	var allResults: Result[] = results.Results;
 	if (allResults.length > 0) {
-		var channel = vscode.window.createOutputChannel('HL7 Fields from all messages');
+		var channel: vscode.OutputChannel = vscode.window.createOutputChannel('HL7 Fields from all messages');
 		channel.clear();
 		channel.appendLine(common.padRight("Value", results.MaxLength, ' ') + "  Filename");
 		channel.appendLine(common.padRight("-----", results.MaxLength, ' ') + "  --------");
-		for (i = 0; i < allResults.length; i++) {
+		for (let i: number = 0; i < allResults.length; i++) {
 			channel.appendLine(common.padRight(allResults[i].Value, results.MaxLength, ' ') + "  " + allResults[i].Filename)
 		}
-		channel.show(vscode.ViewColumn.Two);
+		channel.show();
 	}
 	return ExtractReturnCode.SUCCESS;
 }
-
-exports.ExtractAllFields = ExtractAllFields;
-exports.ExtractReturnCode = ExtractReturnCode;
