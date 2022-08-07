d26cccd6d473c9082fdbebfec436e814541b5ccc
Rob Holme <rob@holme.com.au>
2022-08-07 16:12:50 +0800

    converted to typescript

--- src/CursorManager.ts
+++ src/CursorManager.ts
@@ -2,32 +2,41 @@
     Rob Holme
 
     Class used to get and set cursor position in the current document
-    
+
 */
 
-const vscode = require('vscode');
+import * as vscode from 'vscode';
 
-//----------------------------------------------------
-// class defining a single field value and the filename it was found in
-class CursorManager {
-	// return the current cursor position
-	get CursorPosition() {
-		const editor = vscode.window.activeTextEditor;
-		const position = editor.selection.active;
-		return position;
+
+export abstract class CursorManager {
+	
+	//----------------------------------------------------
+	// Get the cursor position from the active text editor
+	// @retuens {vscode.Position} cursor position
+	public static GetCursorPosition() : vscode.Position | undefined {
+		const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+		if (editor != undefined) {
+			const position = editor.selection.active;
+			return position;
+		}
+		else {
+			return undefined;
+		}
 	}
 
+	//----------------------------------------------------
 	// set the position of the cursor in the document, selecting a range of text. Reveal the cursor position (if not in the viewport).
 	// @param {FindFieldResult} FieldLocation - the line number of the field, and the start and end character positions of the field
-	set CursorPosition(FieldLocation) {
-		const editor = vscode.window.activeTextEditor;
-		var startPosition = new vscode.Position(FieldLocation.Line, FieldLocation.StartCharacter);
-		var endPosition = new vscode.Position(FieldLocation.Line, FieldLocation.EndCharacter);
-		var newSelection = new vscode.Selection(startPosition, endPosition);
-		var newRange = new vscode.Range(startPosition,endPosition);
-		editor.selection = newSelection;
-		editor.revealRange(newRange, vscode.TextEditorRevealType.InCenterIfOutsideViewport);
+	public static SetCursorPosition(FieldLocation: FindFieldResult) {
+		const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+		var startPosition: vscode.Position = new vscode.Position(FieldLocation.Line, FieldLocation.StartCharacter);
+		var endPosition: vscode.Position = new vscode.Position(FieldLocation.Line, FieldLocation.EndCharacter);
+		var newSelection: vscode.Selection = new vscode.Selection(startPosition, endPosition);
+		var newRange: vscode.Range = new vscode.Range(startPosition,endPosition);
+		if (editor != undefined) {
+			editor.selection = newSelection;
+			editor.revealRange(newRange, vscode.TextEditorRevealType.InCenterIfOutsideViewport);
+		}
 	} 
-}
 
-exports.CursorManager = CursorManager;
+}
