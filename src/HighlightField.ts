7a8f49c4e916bc8c5d7b6479055f09882659c5aa
Rob Holme <rob@holme.com.au>
2022-08-09 17:21:48 +0800

    converted to typescript

--- src/HighlightField.ts
+++ src/HighlightField.ts
@@ -1,22 +1,22 @@
 /*  
-    Rob Holme
+	Rob Holme
 
-    Functions to highlight user specified fields on a HL7 message.
+	Functions to highlight user specified fields on a HL7 message.
 */
 
 // load modules
-const vscode = require('vscode');
-const common = require('./common.js');
+import * as vscode from 'vscode';
+import { Delimiter, HashTable, Util } from "./Util";
 
 // the list of fields to highlight
 var fieldSelectionList = [];
 
 // return codes for ExtractAllFields()
-const HighlightFieldReturnCode = {
-	ERROR_NO_LOCATION_PROVIDED: 0,
-	SUCCESS_NO_FIELD_FOUND: 1,
-	SUCCESS_FIELD_FOUND: 2
-}; 
+export enum HighlightFieldReturnCode {
+	ERROR_NO_LOCATION_PROVIDED = 0,
+	SUCCESS_NO_FIELD_FOUND = 1,
+	SUCCESS_FIELD_FOUND = 2
+};
 
 //----------------------------------------------------
 // Highlight the HL7 field(s) identified by the user
@@ -25,18 +25,20 @@ const HighlightFieldReturnCode = {
 // @param {string} backgroundColor - The RGBA colour value identifying the background colour e.g. "rgba(0,255,0,0.3)". If this is not supplied, a default colour will be used.
 //
 // @returns {int} - returns the number of decorations applied
-function ShowHighlights(itemLocation, hl7Schema, backgroundColor) {
+export function ShowHighlights(itemLocation: string, hl7Schema: object, backgroundColor: string) {
 	// the default background colour for highlighted items (if not otherwise specified)
-	const defaultBackgroundColour = 'rgba(0,255,0,0.3)';
+	const defaultBackgroundColour: string = 'rgba(0,255,0,0.3)';
 
-	// load the message delimiters from the current file
-	var delimiters = common.ParseDelimiters();
+	// load the message delimiters from the active editor window
+	var delimiters = new Delimiter;
+	delimiters.ParseDelimitersFromActiveEditor();
+
 	// regex to match a segment name and field index 
-	var itemLocationRegex = new RegExp("^[A-Z]{2}([A-Z]|[0-9])[-]([0-9]{1,3})$", 'i');
+	var itemLocationRegex: RegExp = new RegExp("^[A-Z]{2}([A-Z]|[0-9])[-]([0-9]{1,3})$", 'i');
 	// associative array indexed by segment name, with the field index as a value. 
-	var locationHashtable = {};
-	var highlightBackgroundColour = defaultBackgroundColour;
-	var activeEditor = vscode.window.activeTextEditor;
+	var locationHashtable: HashTable<number[]> = {};
+	var highlightBackgroundColour: string = defaultBackgroundColour;
+	var activeEditor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
 
 	// dispose of decorations for previously highlighted fields. 
 	if (fieldSelectionList.length > 0) {
@@ -55,30 +57,31 @@ function ShowHighlights(itemLocation, hl7Schema, backgroundColor) {
 	}
 
 	// create a decorator type that is used to decorate selected fields
-	var highlightDecorationType = vscode.window.createTextEditorDecorationType({
+	var highlightDecorationType: vscode.TextEditorDecorationType = vscode.window.createTextEditorDecorationType({
 		backgroundColor: highlightBackgroundColour
 	});
 
 	// Test to see if the user has provided a valid location string, otherwise assume a description has been provided instead
 	if (itemLocationRegex.test(itemLocation)) {
 		// identify the segment name and field index from the location string
-		var segmentName = common.GetSegmentNameFromLocationString(itemLocation).toUpperCase();
-		var fieldIndex = parseInt(common.GetFieldIndex(itemLocation), 10);
-		// the first field of MSH, FHS, and BHS segments is the field delimiter, adjust index accordingly for MSH fields
-		if (segmentName == "MSH" || segmentName == "FHS" || segmentName == "BHS") {
-			fieldIndex--;
+		var segmentName: string = Util.GetSegmentNameFromLocationString(itemLocation).toUpperCase();
+		var fieldIndex: number | null = Util.GetFieldIndex(itemLocation);
+		if (fieldIndex != null) {
+			// the first field of MSH, FHS, and BHS segments is the field delimiter, adjust index accordingly for MSH fields
+			if (segmentName == "MSH" || segmentName == "FHS" || segmentName == "BHS") {
+				fieldIndex--;
+			}
+			locationHashtable[segmentName] = [fieldIndex];
 		}
-		locationHashtable[segmentName] = [fieldIndex];
 	}
 	// else assume the user has provided a field description to search for instead of a location.
 	else {
-		locationHashtable = common.FindLocationFromDescription(itemLocation, hl7Schema);
+		locationHashtable = Util.FindLocationFromDescription(itemLocation, hl7Schema);
 	}
 
-	fieldSelectionList = AddDecorations(activeEditor, locationHashtable, delimiters.FIELD)
+	fieldSelectionList = AddDecorations(activeEditor, locationHashtable, delimiters.Field)
 	// warn the user if the field selected does not exist in the message
 	if (fieldSelectionList.length < 1) {
-//		vscode.window.showWarningMessage("A field matching " + itemLocation + " could not be located in the message");
 		return HighlightFieldReturnCode.SUCCESS_NO_FIELD_FOUND;
 	}
 
@@ -96,24 +99,25 @@ function ShowHighlights(itemLocation, hl7Schema, backgroundColor) {
 // @param {char} fieldDelimiter - the field delimiter used by the HL7 message. e.g. '|'
 //
 // @return {object} - returns an array of the decoration objects (background highlighting) for each of the locations identified by the locationHashtable parameter.
-function AddDecorations(activeEditor, locationHashtable, fieldDelimiter) {
-	var positionOffset = 0;
+function AddDecorations(activeEditor: vscode.TextEditor, locationHashtable: HashTable<number[]>, fieldDelimiter: string) {
+	var positionOffset: number = 0;
 	var fieldSelectionListToReturn = [];
-	var fieldRegEx = new RegExp("\\" + fieldDelimiter, "g");
+	var fieldRegEx: RegExp = new RegExp("\\" + fieldDelimiter, "g");
 	// calculate the number of characters at the end of line (<CR>, or <CR><LF>)
-	var currentDoc = activeEditor.document;
-	endOfLineChar = common.GetEOLCharacter(currentDoc);
-	var endOfLineLength = endOfLineChar.length;
+	var currentDoc: vscode.TextDocument = activeEditor.document;
+	var endOfLineChar: string = Util.GetEOLCharacter(currentDoc);
+	var endOfLineLength: number = endOfLineChar.length;
 
-	for (lineIndex = 0; lineIndex < activeEditor.document.lineCount; lineIndex++) {
-		var startPos = null;
-		var endPos = null;
-		var currentLine = activeEditor.document.lineAt(lineIndex).text;
-		var fields = currentLine.split(fieldDelimiter);
+	for (let lineIndex: number = 0; lineIndex < activeEditor.document.lineCount; lineIndex++) {
+		var startPos: vscode.Position | null = null;
+		var endPos: vscode.Position | null = null;
+		var currentLine: string = activeEditor.document.lineAt(lineIndex).text;
+		var fields: string[] = currentLine.split(fieldDelimiter);
 		if (!(locationHashtable[fields[0].toUpperCase()] === undefined)) {
 			var fieldCount = 1;
 			// get the location of field delimiter characters
-			while (match = fieldRegEx.exec(currentLine)) {
+			var match: RegExpExecArray | null = fieldRegEx.exec(currentLine)
+			while (match) {
 				// if the start position was located in the previous iteration, then this must be the end position
 				if (startPos) {
 					endPos = activeEditor.document.positionAt(positionOffset + match.index);
@@ -127,6 +131,7 @@ function AddDecorations(activeEditor, locationHashtable, fieldDelimiter) {
 					startPos = activeEditor.document.positionAt(positionOffset + match.index + 1);
 				}
 				fieldCount++;
+				match = fieldRegEx.exec(currentLine)
 			}
 			// check to see if the field requested was the last field in the segment (i.e. start of field delimiter found, but no further field delimiters).
 			if (startPos) {
@@ -142,5 +147,3 @@ function AddDecorations(activeEditor, locationHashtable, fieldDelimiter) {
 }
 
 
-exports.ShowHighlights = ShowHighlights;
-exports.HighlightFieldReturnCode = HighlightFieldReturnCode;
