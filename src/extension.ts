c61549cb6f97338559b3d9f6a7b94afbb9dd7592
Rob Holme <rob@holme.com.au>
2023-01-03 16:35:46 +0800

    added support to detect HL7 schemas v2.8...v2.9

--- src/extension.ts
+++ src/extension.ts
@@ -46,7 +46,7 @@ const defaultSchemaVersion = "2.7.1";
 function GetHL7Version(hl7Message: string): string | null {
 	var hl7HeaderRegex: RegExp = /^MSH.+$/im;
 	var result: RegExpExecArray | null = hl7HeaderRegex.exec(hl7Message);
-	var supportedSchemas: string[] = ["2.1", "2.2", "2.3", "2.3.1", "2.4", "2.5", "2.5.1", "2.6", "2.7", "2.7.1"];
+	var supportedSchemas: string[] = ["2.1", "2.2", "2.3", "2.3.1", "2.4", "2.5", "2.5.1", "2.6", "2.7", "2.7.1", "2.8", "2.8.1", "2.8.2", "2.9"];
 
 	var delimiters: Delimiter = new Delimiter();
 	delimiters.ParseDelimitersFromMessage(hl7Message);
